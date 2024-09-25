24c6a9db29951503386768ec608e0362b935d192
rohit0407rai <rohit0407rai@gmail.com>
2024-09-25 18:34:31 +0530

    Initial Updates

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Rohit Rai",
+  profile: '/profile.png',
+  designation: "Software Developer",
+  description: "Dynamic and innovative software developer with expertise in full-stack application development and a strong focus on leveraging AI and NLP technologies to drive sustainable solutions. Passionate about creating impactful applications that enhance user experiences and promote efficiency.",
+  email: 'rohit0407rai@gmail.com',
+  phone: '+919333193336',
+  address: 'Mumbai, Maharashtra, India',
+  github: 'https://github.com/rohit0407rai',
+  facebook: 'https://www.facebook.com/profile.php?id=100070252815412',
+  linkedIn: 'https://www.linkedin.com/in/rohit-0407-rai/',
+  twitter: 'https://x.com/RohitRa62115511',
+  stackOverflow: 'https://stackoverflow.com/users/22060759/rohit-rai',
+  leetcode: "https://leetcode.com/u/Rohit_0407/",
+  devUsername: "rohit0407rai",
+  resume: "https://drive.google.com/file/d/1o9f7aHVx_4Jmdm3OFt4eYD4Q3kX3A8HM/view?usp=sharing"
+}
